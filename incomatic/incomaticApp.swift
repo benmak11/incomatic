8fdeda5309b00c813eee05a19a1c10add1039f1d
Ben Makusha <btmakusha@duck.com>
2025-11-09 12:58:56 -0500

    Initial Commit

---
+++ incomatic/incomaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  incomaticApp.swift
+//  incomatic
+//
+//  Created by Ben Makusha on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct incomaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
